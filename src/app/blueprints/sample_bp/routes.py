affc88b6683efa5d5d7f2d488cbe2f6dbcfa95fc
Bryan Bar <bryanbarajas@tutamail.com>
2020-10-04 07:02:34 -0700

    simple endpoint to test nginx proxy

--- src/app/blueprints/sample_bp/routes.py
+++ src/app/blueprints/sample_bp/routes.py
@@ -1,6 +1,7 @@
 """ Personal Pages Blueprint routes """
 from typing import Any
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, jsonify
+from os import getpid
 
 
 bp = Blueprint('sample_bp', __name__,
@@ -8,14 +9,13 @@ bp = Blueprint('sample_bp', __name__,
                static_folder='static',
                static_url_path='/sample_bp/static')
 
-'''
+
 @bp.route('/', methods=['GET'])
 def index() -> Any:
     """
     :return: Any
     """
-    return render_template('index.html', title='Home')
-'''
+    return str(getpid())
 
 
 @bp.route('/ping', methods=['GET'])
